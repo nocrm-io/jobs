4cccbe497f23eba2576e5a6521be6d1d1e5063e1
Güven Urganci <guven.urganci@gmail.com>
2020-10-19 16:00:11 +0200

    root is the leads index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :leads
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: "leads#index"
 end
